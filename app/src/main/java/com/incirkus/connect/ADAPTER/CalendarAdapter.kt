88967d3f5f3fe8447370901460e5a9f3d09557ac
MuhammetIncirkus <164883670+MuhammetIncirkus@users.noreply.github.com>
2024-08-15 16:08:10 +0200

    fixed bugs

--- app/src/main/java/com/incirkus/connect/ADAPTER/CalendarAdapter.kt
+++ app/src/main/java/com/incirkus/connect/ADAPTER/CalendarAdapter.kt
@@ -1,17 +1,23 @@
-package com.incirkus.connect.ADAPTER
+package com.incirkus.customcalendar.adapter
 
+
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.incirkus.connect.DATA.Model.ChatRoom
+import com.incirkus.connect.DATA.Model.Month
+import com.incirkus.connect.R
 import com.incirkus.connect.ViewModel
-import com.incirkus.connect.databinding.ChatListElementBinding
+import com.incirkus.connect.databinding.CalendarListItemBinding
+import java.time.LocalDate
 
-class CalendarAdapter (private var monthList: List<String>, private val viewModel: ViewModel) : RecyclerView.Adapter<CalendarAdapter.ItemViewHolder>() {
-    inner class ItemViewHolder(val binding: ChatListElementBinding) : RecyclerView.ViewHolder(binding.root)
+
+class CalendarAdapter (private var monthList: List<Month>, private val viewModel: ViewModel,) : RecyclerView.Adapter<CalendarAdapter.ItemViewHolder>() {
+    inner class ItemViewHolder(val binding: CalendarListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val binding = ChatListElementBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = CalendarListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ItemViewHolder(binding)
     }
 
@@ -21,7 +27,30 @@ class CalendarAdapter (private var monthList: List<String>, private val viewMode
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val binding = holder.binding
-        val chatRoom = monthList[position]
+        val month = monthList[position]
+
+        binding.tvMonthAndYear.text = "${month.monthString} ${month.year}"
+        val daylist = viewModel.createDayList(month)
+
+        val currentDate = LocalDate.now()
+
+        val context = binding.tvMonthAndYear.context
+        if (month.year == currentDate.year && month.monthNumber == currentDate.month.value){
+            binding.tvMonthAndYear.setTextColor(ContextCompat.getColor(context, R.color.green))
+            binding.tvMonthAndYear.setTypeface(binding.tvMonthAndYear.getTypeface(), Typeface.BOLD)
+            binding.tvMonthAndYear.textSize = 20F
+            binding.tvMonthAndYear.setShadowLayer(1.5f, 2f, 2f, R.color.black)
+        }
+
+        binding.rvDays.setHasFixedSize(true)
+        val monthAdapter = MonthAdapter(daylist, viewModel)
+        binding.rvDays.adapter = monthAdapter
+
+
+
+
+
+
 
     }
 }
---
+++ app/src/main/java/com/incirkus/connect/DATA/Model/Day.kt
@@ -0,0 +1,9 @@
+package com.incirkus.connect.DATA.Model
+
+data class Day(
+    val day: Int,
+    val month: Int,
+    val year: Int,
+    val weekdayAsString: String,
+    val weekdayAsInt: Int
+    )
