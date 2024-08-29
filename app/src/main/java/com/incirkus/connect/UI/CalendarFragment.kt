520dae3f9cb58dbcc5732c225d4e4040e5492bb8
MuhammetIncirkus <164883670+MuhammetIncirkus@users.noreply.github.com>
2024-08-29 11:13:51 +0200

    Adjustments to calendar layout

--- app/src/main/java/com/incirkus/connect/UI/CalendarFragment.kt
+++ app/src/main/java/com/incirkus/connect/UI/CalendarFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.incirkus.connect.ViewModel
 import com.incirkus.connect.databinding.FragmentCalendarBinding
@@ -38,10 +39,11 @@ class CalendarFragment : Fragment() {
         val actualMonth = viewModel.actualMonth
         val startposition = viewModel.monthList.indexOf(actualMonth)
 
-        val viewPager : ViewPager2 = binding.vp2Calendar
+
+        val recyclerView= binding.rvCalendar
         val calendarAdapter = CalendarAdapter(viewModel.monthList, viewModel)
-        viewPager.adapter = calendarAdapter
-        viewPager.setCurrentItem(startposition,false)
+        recyclerView.adapter = calendarAdapter
+        recyclerView.scrollToPosition(startposition)
 
 
         viewModel.currentMonth.observe(viewLifecycleOwner){
@@ -49,42 +51,42 @@ class CalendarFragment : Fragment() {
 
             binding.tvSelectedDay.text = viewModel.currentMonth.value!!.monthString + " " + viewModel.currentMonth.value!!.year
 
-            if(it.year >= viewModel.monthList.first().year){
-                binding.btnYearBack.isClickable = false
-            } else{
-                binding.btnYearBack.isClickable = true
-            }
-
-            if (it.year <=viewModel.monthList.last().year ){
-                binding.btnYearNext.isClickable = false
-            } else{
-                binding.btnYearNext.isClickable = true
-            }
-
-            if (it.year >= viewModel.monthList.first().year && it.monthNumber <= viewModel.monthList.first().monthNumber){
-                binding.btnMonthBack.isClickable = false
-            } else{
-                binding.btnMonthBack.isClickable = true
-            }
-
-            if (it.year <= viewModel.monthList.last().year && it.monthNumber <= viewModel.monthList.last().monthNumber){
-                binding.btnMonthNext.isClickable = false
-            } else{
-                binding.btnMonthNext.isClickable = true
-            }
-
-            binding.btnMonthBack.setOnClickListener {
-                viewModel.monthMinus1(viewModel.currentMonth.value!!)
-            }
-            binding.btnMonthNext.setOnClickListener {
-                viewModel.monthPlus1(viewModel.currentMonth.value!!)
-            }
-            binding.btnYearBack.setOnClickListener {
-                viewModel.yearMinus1(viewModel.currentMonth.value!!)
-            }
-            binding.btnYearNext.setOnClickListener {
-                viewModel.yearPlus1(viewModel.currentMonth.value!!)
-            }
+//            if(it.year >= viewModel.monthList.first().year){
+//                binding.btnYearBack.isClickable = false
+//            } else{
+//                binding.btnYearBack.isClickable = true
+//            }
+//
+//            if (it.year <=viewModel.monthList.last().year ){
+//                binding.btnYearNext.isClickable = false
+//            } else{
+//                binding.btnYearNext.isClickable = true
+//            }
+//
+//            if (it.year >= viewModel.monthList.first().year && it.monthNumber <= viewModel.monthList.first().monthNumber){
+//                binding.btnMonthBack.isClickable = false
+//            } else{
+//                binding.btnMonthBack.isClickable = true
+//            }
+//
+//            if (it.year <= viewModel.monthList.last().year && it.monthNumber <= viewModel.monthList.last().monthNumber){
+//                binding.btnMonthNext.isClickable = false
+//            } else{
+//                binding.btnMonthNext.isClickable = true
+//            }
+//
+//            binding.btnMonthBack.setOnClickListener {
+//                viewModel.monthMinus1(viewModel.currentMonth.value!!)
+//            }
+//            binding.btnMonthNext.setOnClickListener {
+//                viewModel.monthPlus1(viewModel.currentMonth.value!!)
+//            }
+//            binding.btnYearBack.setOnClickListener {
+//                viewModel.yearMinus1(viewModel.currentMonth.value!!)
+//            }
+//            binding.btnYearNext.setOnClickListener {
+//                viewModel.yearPlus1(viewModel.currentMonth.value!!)
+//            }
         }
 
 
